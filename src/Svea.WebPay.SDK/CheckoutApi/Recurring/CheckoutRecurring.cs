13e746f3234075e0a69e5715b0eed5501e2eaa28
Dusan Bobicic <9326334+dusanbobicic@users.noreply.github.com>
2024-10-01 09:54:15 +0200

    Fix Recurring Order Model (#312)
    * Fix Recurring Order Model
    
    * Recurring order model change for GetRecurringOrder

--- src/Svea.WebPay.SDK/CheckoutApi/Recurring/CheckoutRecurring.cs
+++ src/Svea.WebPay.SDK/CheckoutApi/Recurring/CheckoutRecurring.cs
@@ -12,10 +12,10 @@ namespace Svea.WebPay.SDK.CheckoutApi.Recurring
             _sveaHttpClient = sveaHttpClient;
         }
 
-        public async Task<OrderData> CreateRecurringOrderAsync(CreateRecurringOrderModel createRecurringOrder, string recurringToken, bool configureAwait = false)
+        public async Task<RecurringOrder> CreateRecurringOrderAsync(CreateRecurringOrderModel createRecurringOrder, string recurringToken, bool configureAwait = false)
         {
             var url = new Uri($"/api/tokens/{recurringToken}/orders", UriKind.Relative);
-            var data = await _sveaHttpClient.HttpPost<OrderData>(url, createRecurringOrder, configureAwait);
+            var data = await _sveaHttpClient.HttpPost<RecurringOrder>(url, createRecurringOrder, configureAwait);
             return data;
         }
 
@@ -25,10 +25,10 @@ namespace Svea.WebPay.SDK.CheckoutApi.Recurring
             var data = await _sveaHttpClient.HttpGet<RecurringToken>(url, configureAwait);
             return data;
         }
-        public async Task<OrderData> GetRecurringOrderAsync(string recurringToken, long orderId, bool configureAwait = false)
+        public async Task<RecurringOrder> GetRecurringOrderAsync(string recurringToken, long orderId, bool configureAwait = false)
         {
             var url = new Uri($"/api/tokens/{recurringToken}/orders/{orderId}", UriKind.Relative);
-            var data = await _sveaHttpClient.HttpGet<OrderData>(url, configureAwait);
+            var data = await _sveaHttpClient.HttpGet<RecurringOrder>(url, configureAwait);
             return data;
         }
         public async Task<ChangePaymentMethodResponse> ChangePaymentMethodAsync(ChangepaymentMethodModel changePaymentMethodModel, string recurringToken, bool configureAwait = false)
